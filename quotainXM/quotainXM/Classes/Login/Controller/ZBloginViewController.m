cc953cd927d5c1dbe8c35a093eb123ec030d1526
朱彬 <252253401@qq.com>
2020-06-17 09:17:55 +0800

    6.17.1

--- quotainXM/quotainXM/Classes/Login/Controller/ZBloginViewController.m
+++ quotainXM/quotainXM/Classes/Login/Controller/ZBloginViewController.m
@@ -87,8 +87,12 @@
        
 //    });
 
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [MBProgressHUD hideHUD];
+        [self beginLogin];
+    });
     
-    [self beginLogin];
+    
 
     
 
@@ -134,19 +138,20 @@
 
 //    NSLog(@"%@",par);
     
-    
+    /*
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
     [manager POST:@"http://api.yysc.online/system/login" parameters:par headers:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        [MBProgressHUD showMessage:@"登录成功..."];
         //延时执行代码
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-
+            [MBProgressHUD hideHUD];
         HNPPersonVC *vc = [[HNPPersonVC alloc] init];
         [self.navigationController  pushViewController:vc animated:YES];
         });
         } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
             [MBProgressHUD hideHUD];
             [MBProgressHUD showError:@"用户名或密码错"];
-        }];
+        }];*/
 }
 
 
